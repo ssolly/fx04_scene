485331ebbfd2a2192f922c07213b3b7f9ded8653
Choi.SY <sollly0829@gmail.com>
2021-09-03 13:14:34 +0900

    fx04.ex01 주석으로 설명 추가

--- src/ex01/Controller.java
+++ src/ex01/Controller.java
@@ -13,6 +13,8 @@ public class Controller implements Initializable {
 	private Parent root;
 	@FXML TextField fxId;
 	
+	//root(pane)을 저장하는 메소드 
+	//(각 정보 및 변수가 너무 많아서 한번에 전달 후 필요한거 꺼내쓰기, 루트로 활용할 메소드多)
 	public void setRoot(Parent root) {
 		this.root = root;
 	}
@@ -25,7 +27,11 @@ public class Controller implements Initializable {
 
 	public void LoginProc() {
 		System.out.println("로그인 버튼 클릭");
+		
+		//root의 "fxId"를 따라가서 t에 저장 (= fxId.setText();)
+		//다만 @FXML어노테이션 사용해서 사용하지 않는 이유는 컨트롤러는 연결해주는 역할만 하기 위해서이다
 		TextField t = (TextField)root.lookup("#fxId");
+		
 		System.out.println(t.getText());
 		System.out.print("Annotation사용 : " + fxId.getText());
 	}
--- src/ex01/MainClass.java
+++ src/ex01/MainClass.java
@@ -10,17 +10,25 @@ public class MainClass extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
+		//main에서 fxml를 로드하고, fxml에 있는 controller가 생성, 만들어진 컨트롤러에 root저장
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		
+		//1. 파일 로드 : 파일에 대한 정보를 loader에 저장, fxml에서 셋팅했던 controller 가져올 수 있음
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("eventTest.fxml"));
+		
+		//2. 파일 정보를 root(pane)에 저장
 		Parent root = loader.load();	//작성한 내용을 실행시킨다
 		
+		//3. 신에 저장 -> 보여줌
 		Scene scene = new Scene(root);
 		
+		//4. fxml에서 셋팅했떤 controller 셋팅
 		Controller ctl = loader.getController();	//컨트롤러 얻어옴
+		
+		//5. root(pane)을 컨트롤러에 세팅
 		ctl.setRoot(root);
 		
 		primaryStage.setScene(scene);
